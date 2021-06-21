4b42f059e7415417ffd3cd1b635fb7cf3f4986ea
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-06-21 12:47:19 +0200

    created the part for client

--- src/App.java
+++ src/App.java
@@ -1,17 +1,46 @@
 package src;
+
+import java.util.Scanner;
+
 public class App 
 {
     public static void main(String[] args) 
     {
         System.out.println("==============================================================");
         System.out.println("=========                                            =========");
-        System.out.println("====               WELCOME ON OUR RESTAURANT🍖            ====");
+        System.out.println("====          🍺🍗WELCOME IN OUR RESTAURANT🍖🍻          ====");
         System.out.println("=========                                            =========");
         System.out.println("==============================================================");
 
-        // pour acceder a l'administration on va utiliser le nombre 15555
+        // pour acceder a l'administration on va utiliser le nombre 1172
         
         // ====> client
+        String client_first_word = "i wanna ";
+        System.out.println("How can we help you?");
+        System.out.println("1. "+client_first_word+" See the menu");
+        System.out.println("2. "+client_first_word+" Leave the Resto");
+        
+        int choice = 0;
+        do {
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Here's our resto menu!");
+                    break;
+                case 2:
+                    System.out.println("OK, thanks for choosing us! welcome back again😘");
+                    break;
+                default:
+                    System.out.println("I don't know what you mean (0_0)");
+                    break;
+            }
+        } while(choice != 1 && choice != 2);
+        
+        // String client_second_word = "i choose ";
+        // System.out.println("1. "+client_second_word+" See the menu");
+        // System.out.println("2. "+client_second_word+" See the menu");
+
         // **** afficher des options
         /*
             - demander menu (liste des produits dispo)
