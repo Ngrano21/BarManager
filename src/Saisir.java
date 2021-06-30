a50b8a89472c066d880e57e77a73f84e60d02661
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-06-30 11:31:56 +0200

    done the second test

--- src/Saisir.java
+++ src/Saisir.java
@@ -1,7 +1,4 @@
-
-import java.util.*;
 import java.io.*;
-import java.lang.Thread;
 
 public class Saisir {
   //Scanner nt gera am IOException
@@ -11,71 +8,74 @@ public class Saisir {
 
 
 
- public static int Saissir_Entier() throws IOException {
-	BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
-	 int a=0;
-	 String b="";
-	   if(sc!=null){
-      	 System.out.print(" ");
-	 do {
-		 try {
-			 b=sc.readLine();
-			 a=Integer.parseInt(b);
-			 break;
-		 }catch(IOException e) {
-			 e.printStackTrace();
-		 }
-		 
-		 catch(NumberFormatException et) {
-			 System.out.println("Entrez l'entier S'il Vous Plait!!!");
-		 }
-	 }while(true);
+	public static int Saissir_Entier() throws IOException {
+		BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
+		int a=0;
+		String b="";
+			if(sc!=null){
+				System.out.print(" ");
+				do {
+					try {
+						b=sc.readLine();
+						a=Integer.parseInt(b);
+						break;
+					}
+					catch(IOException e) {
+						e.printStackTrace();
+					}
+				
+					catch(NumberFormatException et) {
+						System.out.println("Entrez un entier S'il Vous Plait!!!");
+					}
+				}while(true);
 
-	return a;
-	   }
-	return a;
- }
- public static float Saissir_Reel() throws IOException {
-	BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
-	 float c=0;
-	 String b="";
-	 if(sc!=null){
-      	 System.out.print(" ");
-	 do {
-		 try {
-			 b=sc.readLine();
-			 c=Float.parseFloat(b);
-			 break;
-		 }catch(IOException e) {
-			 e.printStackTrace();
-		 }
-		 
-		 catch(NumberFormatException et) {
-			 System.out.println("Entrez un Reel S'il Vous Plait!!!!");
-		 }
-	 }while(true);
+				return a;
+		}
+		else {
+				return a;
+		}
+	}
+	public static float Saissir_Reel() throws IOException {
+		BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
+		float c=0;
+		String b="";
+		if(sc != null){
+			System.out.print(" ");
+			do {
+				try {
+					b=sc.readLine();
+					c=Float.parseFloat(b);
+					break;
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			
+				catch(NumberFormatException et) {
+					System.out.println("Entrez un Reel S'il Vous Plait!!!!");
+				}
+			}while(true);
 
-	return c;
-	 }
-	return c; 
- }
- public static String Saisie_Caracteres() {
-	BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
- 	String b="";
- 	if(sc!=null){
-     	 System.out.print(" ");
-     	 try {
+			return c;
+		}
+		else {
+			return c; 
+		}
+	}
+	public static String Saisie_Caracteres() {
+		BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
+		String b="";
+		if(sc!=null){
+			System.out.print(" ");
+			try {
 				b=sc.readLine();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
- 	return b;
- }
- 
-	return b;
- }
-}
- 
+			}	
+			return b;
+		}
+		else {
+			return b;
+		}
+	}
 
+}
